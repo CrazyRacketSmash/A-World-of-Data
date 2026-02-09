b395da65594dab70910287b9e2cbf7fc3d054573
HN <nguye2cy@mail.uc.edu>
2026-02-09 10:49:43 -0500

    Initiate project files and add CO2_Histogram

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const width = 700;
+const height = 400;
+const margin = { top: 30, right: 30, bottom: 50, left: 60 };
+
+const co2Svg = d3
+  .select("#co2-histogram")
+  .attr("width", width)
+  .attr("height", height);
+
+d3.csv("data/co2_life_expectancy.csv").then(data => {
+  data.forEach(d => {
+    d.co2_per_capita = +d.co2_per_capita;
+    d.life_expectancy = +d.life_expectancy;
+  });
+
+  drawCO2Histogram(co2Svg, data, width, height, margin);
+});
