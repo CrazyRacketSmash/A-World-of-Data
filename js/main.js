82f267c207162600ea623689d2564c2cc29704d0
HN <nguye2cy@mail.uc.edu>
2026-02-16 20:16:43 -0500

    Add choropleth Map and refactor html and main

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const width = 700;
+const width = 600;
 const height = 400;
 const margin = { top: 30, right: 30, bottom: 50, left: 60 };
 
@@ -17,13 +17,29 @@ const scatterPlotSvg = d3
   .attr("width", width)
   .attr("height", height);
 
-d3.csv("data/co2_life_expectancy.csv").then(data => {
-  data.forEach(d => {
-    d.co2_per_capita = +d.co2_per_capita;
-    d.life_expectancy = +d.life_expectancy;
-  });
+const co2MapSvg = d3
+  .select("#co2-map")
+  .attr("width", width)
+  .attr("height", height);
 
-  drawCO2Histogram(co2Svg, data, width, height, margin);
-  drawLifeExpectancyHistogram(lifeExpectancySvg, data, width, height, margin);
-  drawScatterPlot(scatterPlotSvg, data, width, height, margin);
+const lifeMapSvg = d3
+  .select("#life-map")
+  .attr("width", width)
+  .attr("height", height);
+
+Promise.all([
+  d3.csv("data/co2_life_expectancy.csv"),
+  d3.json("data/world.geojson")
+]).then(([data, geoData]) => {
+
+    data.forEach(d => {
+      d.co2_per_capita = +d.co2_per_capita;
+      d.life_expectancy = +d.life_expectancy;
+    });
+
+    drawCO2Histogram(co2Svg, data, width, height, margin);
+    drawLifeExpectancyHistogram(lifeExpectancySvg, data, width, height, margin);
+    drawScatterPlot(scatterPlotSvg, data, width, height, margin);
+    drawChoropleth(co2MapSvg, geoData, data, "co2_per_capita", d3.interpolateBlues, width, height);
+    drawChoropleth(lifeMapSvg, geoData, data, "life_expectancy", d3.interpolateGreens, width, height);
 });
