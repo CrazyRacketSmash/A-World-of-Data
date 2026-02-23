abc92b466ab84bc96d7367e7ae54d2bbcedf2423
HN <nguye2cy@mail.uc.edu>
2026-02-23 14:09:37 -0500

    Added legends and clean up code

--- js/life_choroplethMap.js
+++ js/life_choroplethMap.js
@@ -14,9 +14,11 @@ function drawLifeChoropleth(svg, geoData, data, attribute, colorScheme, width, h
 
   const values = data.map(d => d[attribute]);
 
-  const color = d3.scaleSequential()
-    .domain(d3.extent(values))
-    .interpolator(colorScheme);
+  const thresholds = [55, 65, 75, 80];
+
+  const color = d3.scaleThreshold()
+    .domain(thresholds)
+    .range(d3.schemeGreens[thresholds.length + 1]);
 
   const brush = d3.brush()
   .extent([[0, 0], [width, height]])
@@ -106,4 +108,34 @@ function drawLifeChoropleth(svg, geoData, data, attribute, colorScheme, width, h
       return value ? color(value) : "#ccc";
     })
     .attr("stroke", "#fff");
+    const legendData = color.range().map((c, i) => {
+    return {
+      color: c,
+      label:
+        i === 0
+          ? `< ${thresholds[0]}`
+          : i === thresholds.length
+            ? `≥ ${thresholds[i - 1]}`
+            : `${thresholds[i - 1]} – ${thresholds[i]}`
+    };
+  });
+
+  const legend = svg.append("g")
+    .attr("transform", `translate(${width - 550}, 220)`);
+
+  legend.selectAll("rect")
+    .data(legendData)
+    .join("rect")
+    .attr("y", (d, i) => i * 22)
+    .attr("width", 18)
+    .attr("height", 18)
+    .attr("fill", d => d.color);
+
+  legend.selectAll("text")
+    .data(legendData)
+    .join("text")
+    .attr("x", 25)
+    .attr("y", (d, i) => i * 22 + 13)
+    .text(d => d.label)
+    .attr("font-size", "12px");
 }
